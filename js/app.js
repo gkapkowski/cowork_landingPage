09ef706cca0e7fcc6a6858aa2650734e743207cc
nmkedziora <n.m.kedziora@gmail.com>
2016-04-18 21:21:16 +0200

    Changed fore and back pictures, added parallax on header

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+
+  $(window).on("scroll", function () {
+    var windowScroll = $(this).scrollTop();
+    console.log(windowScroll);
+
+    $(".logo").css ({
+      "transform": "translate(0px, "+ windowScroll / 2 + "%)"
+    });
+
+    $(".back-image").css ({
+      "transform": "translate(0px, "+ windowScroll / 8 + "%)"
+    });
+
+    $(".fore-image").css ({
+      "transform": "translate(0px, -"+ windowScroll / 20 + "%)"
+    });
+  });
+});
