7d14d1e444d81a9d40bd10ab468c9b46439ccb55
nmkedziora <n.m.kedziora@gmail.com>
2016-04-19 09:24:51 +0200

    Put header parallax effect in function, switched of header silhouettes for devices smaller than 750px

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 $(document).ready(function() {
 
+  headerParallax();
 
-  $(window).on("scroll", function () {
-    var windowScroll = $(this).scrollTop();
-    console.log(windowScroll);
+  // Header - Parallax effect
+  function headerParallax() {
+    $(window).on("scroll", function () {
+      var windowScroll = $(this).scrollTop();
+      console.log(windowScroll);
 
-    $(".logo").css ({
-      "transform": "translate(0px, "+ windowScroll / 3 + "%)"
+      $(".logo").css({
+        "transform": "translate(0px, "+ windowScroll / 3 + "%)"
+      });
+
+      $(".back-image").css({
+        "transform": "translate(0px, "+ windowScroll / 8 + "%)"
+      });
+
+      $(".fore-image").css({
+        "transform": "translate(0px, -"+ windowScroll / 20 + "%)"
+      });
     });
-
-    $(".back-image").css ({
-      "transform": "translate(0px, "+ windowScroll / 8 + "%)"
-    });
-
-    $(".fore-image").css ({
-      "transform": "translate(0px, -"+ windowScroll / 20 + "%)"
-    });
-  });
+  }
 });
